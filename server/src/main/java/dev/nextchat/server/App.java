dce06d355f036dcf80d795bad6ada8447a95d1b8
phuclhp1922 <nguyensongthienphuc@gmail.com>
2025-03-15 08:58:02 +0700

    Connect Java to MySQL. Successful

--- server/src/main/java/dev/nextchat/server/App.java
+++ server/src/main/java/dev/nextchat/server/App.java
@@ -3,12 +3,23 @@
  */
 package dev.nextchat.server;
 
+import java.sql.DriverManager;
+import java.sql.Connection;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/chatdb",
+                "root", "aio2024");
+        } 
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
