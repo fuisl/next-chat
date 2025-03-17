061b0ed0ece8316c3277f8cf1af1fd9777ebab2f
nhientruong04 <truonghaonhien2822004@gmail.com>
2025-03-17 23:59:22 +0700

    test: first commit with Spring Boot

--- server/src/main/java/dev/nextchat/server/App.java
+++ server/src/main/java/dev/nextchat/server/App.java
@@ -3,12 +3,20 @@
  */
 package dev.nextchat.server;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+// @ComponentScan(basePackages = {"com.chatapp.config", "com.chatapp.model", "com.chatapp.repository", "com.chatapp.service", "com.chatapp.controller"})
+// @EnableMongoRepositories(basePackages = "com.chatapp.repository")
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        SpringApplication.run(App.class, args);
     }
 }
