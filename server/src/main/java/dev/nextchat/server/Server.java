c8af70289eb50bffa97904a88464b05902d57694
fuisl <135719107+fuisl@users.noreply.github.com>
2025-03-18 17:38:21 +0700

    refactor: migrating to Spring Boot.

--- server/src/main/java/dev/nextchat/server/Server.java
+++ server/src/main/java/dev/nextchat/server/Server.java
@@ -1,15 +1,30 @@
 package dev.nextchat.server;
 
-import java.io.*;
-import java.net.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Server {
     private static final int THREAD_POOL_SIZE = 10;
     private static final int PORT = 5001;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        SpringApplication.run(Server.class, args);
+
+        try {
+            startServer();
+        } catch (IOException e) {
+            System.err.println("Error starting server: " + e.getMessage());
+        }
+    }
+
+    public static void startServer() throws IOException {
         ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_POOL_SIZE, new ClientThreadFactory());
         ServerSocket serverSocket = new ServerSocket(PORT);
 
