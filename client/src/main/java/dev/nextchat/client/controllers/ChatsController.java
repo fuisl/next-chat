eb26af1863f1f9b45a7f841050f91f25d0acb0a5
Andrw <anpd2004@gmail.com>
2025-03-22 11:40:12 +0700

    IDK wat I did

--- client/src/main/java/dev/nextchat/client/controllers/ChatsController.java
+++ client/src/main/java/dev/nextchat/client/controllers/ChatsController.java
@@ -1,10 +1,12 @@
 package dev.nextchat.client.controllers;
 
 import dev.nextchat.client.models.Model;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.text.Text;
-
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -15,17 +17,16 @@ public class ChatsController implements Initializable {
     public Button fav_btn;
 
     public Text user_name;
-    public Label fusername;
-    public Label text_msg;
-    public Label sent_date;
-    public Button chat_btn;
+    public ContextMenu contextMenu;
+    public MenuItem starredMessages;
+    public MenuItem newGroup;
+    public MenuItem newMess;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         addListeners();
     }
     private void addListeners() {
-        chat_btn.setOnAction(event -> onChatMess());
     }
     private void onChatMess() {
         Model.getInstance().getViewFactory().getClientSelectedChat().set("Messages");
