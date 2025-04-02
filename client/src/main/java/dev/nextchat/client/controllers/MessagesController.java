26f99e29e844f8931345b0d0c21b902e1c7eb48f
Andrw <anpd2004@gmail.com>
2025-04-02 17:46:58 +0700

    feat: Update UI of sent messages

--- client/src/main/java/dev/nextchat/client/controllers/MessagesController.java
+++ client/src/main/java/dev/nextchat/client/controllers/MessagesController.java
@@ -5,8 +5,13 @@ import dev.nextchat.client.models.Model;
 import dev.nextchat.client.models.Message;
 import java.time.LocalDateTime;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -20,12 +25,17 @@ public class MessagesController implements Initializable {
     public Button send_btn;
     public VBox chatContainer;
     public Label Fid;
+    public ScrollPane sp_main;
     private String fusername;
 
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        chatContainer.heightProperty().addListener((observable, oldValue, newValue) -> {
+            sp_main.setVvalue((Double) newValue);
+        });
+
         send_btn.setOnAction(e -> {
             String sender = "me"; // Replace with: Model.getInstance().getCurrentUser().getUsername()
             String receiver = Fid.getText();
@@ -36,10 +46,26 @@ public class MessagesController implements Initializable {
             cell.txtMsgProperty().set(msg);
             cell.timestampProperty().set(message.getTimestamp());
 
-            Label messageLabel = new Label("Me: " + msg);
-            messageLabel.setWrapText(true);
-            messageLabel.setStyle("-fx-background-color: lightblue; -fx-padding: 10; -fx-background-radius: 8;");
-            chatContainer.getChildren().add(messageLabel);
+            if (!msg.isEmpty()){
+                HBox hbox = new HBox();
+                hbox.setAlignment(Pos.CENTER_RIGHT);
+                hbox.setPadding(new Insets(5,5,-3,10));
+                Text text = new Text(msg);
+                TextFlow textFlow = new TextFlow(text);
+
+                textFlow.setStyle("-fx-color: white;" +
+                        " -fx-background-color: #3a54fb;" +
+                        "-fx-background-radius: 15px;" +
+                        "-fx-font-size: 14px;");
+
+                textFlow.setPadding(new Insets(5,10,5,10));
+                text.setFill(Color.color(0.934,0.945,0.996));
+
+                hbox.getChildren().add(textFlow);
+                chatContainer.getChildren().add(hbox);
+
+            }
+
 
             msg_inp.clear();
             System.out.println("Built message: " + message);
