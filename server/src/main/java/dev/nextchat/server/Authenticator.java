7b7eb111a11bf96fb2d78b2abc212fedb1957f65
phuclhp1922 <nguyensongthienphuc@gmail.com>
2025-04-02 21:15:07 +0700

    Move from JDBC to Spring Boot. Seems no more error. NOT TESTED, BUILT, OR TRIED YET.

--- server/src/main/java/dev/nextchat/server/Authenticator.java
+++ server/src/main/java/dev/nextchat/server/Authenticator.java
@@ -1,75 +1,32 @@
 package dev.nextchat.server;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLIntegrityConstraintViolationException;
-import java.sql.SQLException;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.Optional;
 
-public final class Authenticator {
-    private static Authenticator instance;
-    private Connection connection;
-    private PreparedStatement statement; 
+@Service
+public class Authenticator {
 
-    private Authenticator() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/chatdb",
-                "root", "aio2024");
-        } 
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-    
-    } 
+    @Autowired
+    private UserRepository userRepository;
 
-    public static Authenticator getInstance() {
-        if (instance == null) {
-            instance = new Authenticator();
-        }
-        return instance;
-    } 
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-    public boolean signIn(Credential cred) { 
-        String query = "SELECT user_password FROM user_account WHERE username = ?";
-
-        try {
-            statement = connection.prepareStatement(query); 
-            statement.setString(1, cred.getUsername()); 
-
-            ResultSet resultSet = statement.executeQuery(); 
-
-            if (resultSet.next() && resultSet.getString("user_password").equals(cred.getHashedPassword())) {
-                return true; 
-            }
-        } 
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return false; 
+    public boolean signIn(Credential cred) {
+        Optional<User> userOpt = userRepository.findByUsername(cred.getUsername());
+        return userOpt.isPresent() && passwordEncoder.matches(cred.getHashedPassword(), userOpt.get().getPassword());
     }
 
-    public boolean signUp(Credential cred) throws SQLException {
-        String query = "INSERT INTO user_account (username, user_password) VALUES (?, ?)";
-
-        try {
-            statement = connection.prepareStatement(query);
-            statement.setString(1, cred.getUsername());
-            statement.setString(2, cred.getHashedPassword());
-
-            statement.executeUpdate();
-
-            return true; 
-        } 
-        catch (SQLIntegrityConstraintViolationException e) {
-            e.printStackTrace();
+    @Transactional
+    public boolean signUp(Credential cred) {
+        if (userRepository.existsByUsername(cred.getUsername())) {
+            return false;
         }
 
-        return false; 
-
+        User newUser = new User(cred.getUsername(), passwordEncoder.encode(cred.getHashedPassword()));
+        userRepository.save(newUser);
+        return true;
     }
-
 }
