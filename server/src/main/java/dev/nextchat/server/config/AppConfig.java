1319ec9db9d5c0a351247090d0b77c30f1cb1613
fuisl <135719107+fuisl@users.noreply.github.com>
2025-04-17 12:48:41 +0700

    feat: config DB for repositories.

--- server/src/main/java/dev/nextchat/server/config/AppConfig.java
+++ server/src/main/java/dev/nextchat/server/config/AppConfig.java
@@ -3,8 +3,22 @@ package dev.nextchat.server.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
+@EnableJpaRepositories(
+    basePackages = {
+        "dev.nextchat.server.auth.repository",
+        "dev.nextchat.server.session.repository",
+        "dev.nextchat.server.group.repository"
+    }
+)
+@EnableMongoRepositories(
+    basePackages = {
+        "dev.nextchat.server.messaging.repository"
+    }
+)
 public class AppConfig {
 
     @Bean
