0bbc6b9a08a5d32eb273d5f3d9ee1b477482b5d5
Andrw <anpd2004@gmail.com>
2025-05-26 04:14:18 +0700

    Added: FetchBefore groupId in response

--- server/src/main/java/dev/nextchat/server/protocol/impl/FetchBeforeMessageCommand.java
+++ server/src/main/java/dev/nextchat/server/protocol/impl/FetchBeforeMessageCommand.java
@@ -60,6 +60,7 @@ public class FetchBeforeMessageCommand implements Command {
             msg.put("id", m.getId().toString());
             msg.put("senderId", m.getSenderId().toString());
             msg.put("senderUsername", m.getSenderUsername());
+            msg.put("groupId", m.getGroupId().toString());
             msg.put("content", m.getContent());
             msg.put("timestamp", m.getTimestamp().toString());
             jsonMessages.put(msg);
